e16d3d15923f85f9ef8b835a7c5746895ebdf477
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2016-11-16 19:58:22 +0100

    Removed files from remote git and remove comments

--- kmeans/kmeans_test.go
+++ kmeans/kmeans_test.go
@@ -11,11 +11,9 @@ func TestKMeans(t *testing.T) {
 	kmeans := NewKMeans(3, 3, false)
 	data := [][]float64{[]float64{0, 0, 0},
 		[]float64{2, 2, 2}}
-	//fmt.Println(kmeans.Centroids)
 	err := kmeans.Fit(data)
 	ensure.Nil(t, err)
 	for i, element := range kmeans.PointsAssignment {
 		fmt.Printf("%v %v\n", i, element)
 	}
-	//fmt.Println(kmeans.Centroids)
 }
