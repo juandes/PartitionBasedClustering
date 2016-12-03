da10348197864a81c0f90a9271c2a13d05330dc9
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2016-12-03 18:02:39 +0100

    Implemented intercluster distance for kmeans

--- kmeans/kmeans.go
+++ kmeans/kmeans.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"reflect"
 
+	"log"
 	"partitionclustering/centroid"
 	"partitionclustering/datapoint"
 	"partitionclustering/distances"
@@ -23,7 +24,18 @@ type KMeans struct {
 	Centroids        []centroid.Centroid
 	Datapoints       []datapoint.Datapoint
 	PointsAssignment []int
+	ClusterDistance  [][]float64
 	initRandom       bool
+}
+
+// Result is made of the results of the clustering
+type Result struct {
+	NumClusters      int
+	PointsAssignment []int
+
+	// Cluster validation
+	// InterclusterDistance is the distance between all the clusters
+	InterclusterDistance float64
 }
 
 // NewKMeans creates a KMeans object
@@ -41,8 +53,24 @@ func NewKMeans(numClusters int, numFeatures int, initRandom bool) *KMeans {
 	}
 }
 
+func (km *KMeans) newResult() *Result {
+	return &Result{
+		NumClusters:          km.NumClusters,
+		PointsAssignment:     km.PointsAssignment,
+		InterclusterDistance: km.interclusterDistance(),
+	}
+}
+
 // Fit trains the KMeans model
-func (km *KMeans) Fit(data [][]float64) error {
+func (km *KMeans) Fit(dataset [][]float64) (*Result, error) {
+	data := make([][]float64, len(dataset))
+	// Copy the array
+	for i := range dataset {
+		for j := range dataset[i] {
+			data[i] = append(data[i], dataset[i][j])
+		}
+	}
+
 	previousAssignments := make([]int, len(data))
 
 	// Use a random point from the original dataset as centroid
@@ -51,11 +79,11 @@ func (km *KMeans) Fit(data [][]float64) error {
 	}
 
 	if len(data) < 1 {
-		return fmt.Errorf("Dataset is empty")
+		return nil, fmt.Errorf("Dataset is empty")
 	}
 
 	if len(data[0]) != km.NumFeatures {
-		return fmt.Errorf("Number of dimensions is distinct than the number of features")
+		return nil, fmt.Errorf("Number of dimensions is distinct than the number of features")
 	}
 
 	iterations := 0
@@ -72,10 +100,9 @@ func (km *KMeans) Fit(data [][]float64) error {
 	fmt.Printf("Number of iterations: %v\n", iterations)
 	fmt.Printf("Clustering vector: %v\n", km.PointsAssignment)
 
-	for _, val := range km.PointsAssignment {
-		fmt.Printf("%v,", val)
-	}
-	return nil
+	km.ClusterDistance = km.clustersDistanceMatrix(dataset)
+
+	return km.newResult(), nil
 }
 
 // Assigns each datapoint to a cluster
@@ -84,7 +111,6 @@ func (km *KMeans) assign(data [][]float64) {
 
 	// Step 1: Assign each point to its nearest centroid
 	for i, entry := range data {
-		//fmt.Printf("Features: %v\n", features)
 		currentCentroid := -1
 		currentDistance := math.MaxFloat64
 
@@ -113,15 +139,66 @@ func (km *KMeans) assign(data [][]float64) {
 		km.PointsAssignment[i] = currentCentroid
 	}
 
-	//fmt.Printf("CC: %v\n", km.PointsAssignment)
-	//km.Datapoints = datapoints
-
 }
 
 func (km *KMeans) updateCentroids() {
 	for _, centroid := range km.Centroids {
 		for j := 0; j < km.NumFeatures; j++ {
-			centroid.Position[j] = centroid.DimensionSums[j] / float64(centroid.TimesItWon)
+			// This occurence means that the centroid has never the closest to any of the datapoints
+			if centroid.TimesItWon != 0 {
+				centroid.Position[j] = centroid.DimensionSums[j] / float64(centroid.TimesItWon)
+			}
 		}
 	}
+}
+
+// This function returns a matrix with the distances between clusters.
+// It is calculated by getting the distance between all the points that belongs
+// to different clusters
+func (km *KMeans) clustersDistanceMatrix(data [][]float64) [][]float64 {
+	// Create empty matrix
+	clusterDistanceMatrix := make([][]float64, km.NumClusters)
+	for i := range clusterDistanceMatrix {
+		clusterDistanceMatrix[i] = make([]float64, km.NumClusters)
+	}
+
+	// Calculate the distance between all the points.
+	// Since the distance between A and B is the same as B and A, aka dist(A,B) = dist(B,A)
+	// we avoid calculating the second case
+	// Once the distance is calculated, its result is added to the distance matrix
+	for j := 0; j < len(km.PointsAssignment); j++ {
+		for k := j; k < len(km.PointsAssignment); k++ {
+			// Avoid calculating the distance between the same points, aka dist(A,A)
+			if j == k || km.PointsAssignment[j] == km.PointsAssignment[k] {
+				continue
+			}
+
+			distance, err := distances.EuclideanDistance(data[j], data[k])
+			if err != nil {
+				log.Fatalf("Error: %v", err)
+			}
+
+			// Because dist(A,B) = dist(B,A). The resulting matrix is symmetric with 0
+			// along the diagonal
+			clusterDistanceMatrix[km.PointsAssignment[k]][km.PointsAssignment[j]] += distance
+			clusterDistanceMatrix[km.PointsAssignment[j]][km.PointsAssignment[k]] += distance
+
+		}
+	}
+	return clusterDistanceMatrix
+}
+
+// Calculate the intercluster distance of the model.
+// Intercluster distance is the distance between the clusters
+func (km *KMeans) interclusterDistance() float64 {
+	sum := 0.0
+	// Since the matrix and symmetric, and the diagonal is 0,
+	// only the lower diagonal part is used for calculating the
+	// intercluster distance
+	for i := 1; i < km.NumClusters; i++ {
+		for j := 0; j < i; j++ {
+			sum += km.ClusterDistance[i][j]
+		}
+	}
+	return sum
 }
--- kmeans/kmeans_test.go
+++ kmeans/kmeans_test.go
@@ -1,7 +1,6 @@
 package kmeans
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/facebookgo/ensure"
@@ -11,9 +10,38 @@ func TestKMeans(t *testing.T) {
 	kmeans := NewKMeans(3, 3, false)
 	data := [][]float64{[]float64{0, 0, 0},
 		[]float64{2, 2, 2}}
-	err := kmeans.Fit(data)
+	_, err := kmeans.Fit(data)
 	ensure.Nil(t, err)
-	for i, element := range kmeans.PointsAssignment {
-		fmt.Printf("%v %v\n", i, element)
-	}
+	ensure.DeepEqual(t, kmeans.PointsAssignment, []int{0, 0})
+	ensure.DeepEqual(t, kmeans.Centroids[0].Position, []float64{2.0, 2.0, 2.0})
+}
+
+func TestClusterDistanceMatrix(t *testing.T) {
+	kmeans := NewKMeans(3, 2, false)
+	ab := [][]float64{[]float64{0, 0},
+		[]float64{2, 2},
+		[]float64{3.0, 3.0},
+		[]float64{3.0, 3.0}}
+	data := ab
+	_, err := kmeans.Fit(data)
+	ensure.Nil(t, err)
+
+	cdm := kmeans.ClusterDistance
+
+	ensure.True(t, cdm[0][0] == 0.0)
+	ensure.True(t, cdm[1][1] == 0.0)
+	ensure.True(t, cdm[2][2] == 0.0)
+}
+
+func TestInterclusterDistance(t *testing.T) {
+	kmeans := NewKMeans(3, 2, false)
+	data := [][]float64{[]float64{0, 0},
+		[]float64{2, 2},
+		[]float64{3.0, 3.0},
+		[]float64{3.0, 3.0}}
+	result, err := kmeans.Fit(data)
+	ensure.Nil(t, err)
+
+	icd := result.InterclusterDistance
+	ensure.True(t, icd > 11.31 && icd < 11.32)
 }
